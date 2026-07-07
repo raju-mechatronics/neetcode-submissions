10edbf114f4f7df1ab2463b97ef60a3bef1b49f9
Raju Ahmed <73926176+raju-mechatronics@users.noreply.github.com>
2026-07-07 15:02:24 +0600

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.go
@@ -0,0 +1,15 @@
+func majorityElement(nums []int) int {
+	count:= 0
+	num := 0
+	for i:=0; i< len(nums); i++ {
+		if count ==0 {
+			num = nums[i]
+		}
+		if num == nums[i] {
+			count++
+		}else {
+			count--
+		}
+	}
+	return num
+}
