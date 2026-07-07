8638ef667424d0cb5b12d729637285c22c3e4b85
Raju Ahmed <73926176+raju-mechatronics@users.noreply.github.com>
2026-07-07 11:45:43 +0600

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.go
@@ -0,0 +1,13 @@
+func removeElement(nums []int, val int) int {
+	cur:= 0
+	for i:= 0; i< len(nums); i++ {
+		if nums[i] == val {
+			continue;
+		}
+		if i != cur {
+			nums[cur] = nums[i]
+		}
+		cur++
+	}
+	return cur
+}
